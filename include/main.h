30718f57a301efd351e3aa09dd4660574eb09f59
bryceberwald <bryce@berwald.io>
2023-01-30 16:12:16 -0800

    Added cpp template src code

---
+++ include/main.h
@@ -0,0 +1,10 @@
+#define main_h
+#ifndef man_h
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/**********************************************************
+* Project: 1
+* Team Members: Bryce, Julien, Allen, Adam, Austin
+* Product Owner: Julien
+* Scrum Master: Bryce
+***********************************************************/
+
+#include "../include/main.h"
+
+int main(void){
+
+    cout << "\nProgram being testing...\n";
+
+    return 0;
+}
